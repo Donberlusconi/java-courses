563b36284d12bc0415aed7d9c2f3c267345463ba
Nicat <nicathasan@gmail.com>
2017-05-13 17:10:01 +0400

    test

--- Calculate.java
+++ Calculate.java
@@ -3,6 +3,30 @@
  */
 public class Calculate {
     public static void main(String[] arg){
-        System.out.println("Calculating...");
+
+        int first = Integer.valueOf(arg[0]);
+        int second = Integer.valueOf(arg[1]);
+        double result;
+        String delimiter = "";
+        delimiter = String.valueOf(arg[2]) ;
+
+        if (delimiter.equals("+")) {
+            result = first + second;
+        }
+
+        else if (delimiter.equals("-")){
+            result = first - second;
+        }
+
+        else if (delimiter.equals("/")){
+            result = first/second;
+        }
+
+        else
+        {
+            result = first*second;
+        }
+
+        System.out.println("Result is: " + result + " Delimiter is: " + delimiter);
     }
 }
